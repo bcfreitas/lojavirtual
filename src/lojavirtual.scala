58c42e7647d4e9ab278d45971f1a9c240aaca1e8
Bruno Freitas <bcfreitas@gmailcom>
2015-06-28 19:40:27 -0300

    com autenticacao

--- src/lojavirtual.scala
+++ src/lojavirtual.scala
@@ -9,12 +9,17 @@ object lojavirtual {
   
     val produtos = List("Smartphone", "TV")
     var produtosBD = List(("Smartphone", 100, 2), ("TV", 200, 1))
+    val usuarios = List("Joao", "Maria")
+    val usuariosBD = Map("Joao" -> "123", "Maria" -> "345")
   
   //respostaProdutoBD dividido em respostaPrecoBD e respostaEstoqueBD.
-  def ACESSOCLIENTE(i: Int, consultaProdutoBD: ![String], respostaPrecoBD: ?[Int], respostaEstoqueBD: ?[Int], separaEstoqueBD: ![String], respostaSeparaEstoqueBD: ?[Boolean] ) = proc {
+  def ACESSOCLIENTE(i: Int, consultaProdutoBD: ![String], respostaPrecoBD: ?[Int], respostaEstoqueBD: ?[Int], separaEstoqueBD: ![String], 
+                      respostaSeparaEstoqueBD: ?[Boolean], reverteEstoqueBD: ![String], autenticaUsuario: ![String], enviaSenha: ![String], retornoLogin: ?[Boolean] ) = proc {
     println("acessoCliente." + i)
     //o sleep abaixo é pra garantir seeds diferentes da randomização
     Thread.sleep(i * 300)
+    
+    var carrinho = List[(String,Int,Int)]()
     
     while(true){
       //randomizacao para acesso a produto.
@@ -59,22 +64,90 @@ object lojavirtual {
         
         var retorno = respostaSeparaEstoqueBD? ;
         retorno match {
-          case true => println("enviaProduto")
+          case true => 
+             {
+               println("produto Adicionado no carrinho")
+               carrinho = (produtoAcessado, preco, 1)::carrinho
+              }
           case false => println("Estoque indisponível")
         }
       } else { 
         println("Estoque indisponivel")
       }
       
+      if(carrinho.length > 0){
+        //se houver itens no carrinho, o cliente podera remover, navegar ou concluir compra.
+        var seed = new Random(System.currentTimeMillis())
+        var randomInt = 0
+        randomInt = seed.nextInt(9)
+        
+        randomInt%3 match {
+          case 0 => {
+            println("concluirCompra." + i)
+            
+            var logado = false;
+            //convencionei acesso 1 = joao e acesso 2 = maria
+            
+            while(!logado){
+              println("autenticaUsuario." + i + "." + usuarios(i-1))
+              autenticaUsuario!usuarios(i-1)
+              
+              //randomizando para enviar senha
+              var senha = ""
+              var seed = new Random(System.currentTimeMillis())
+              var randomInt = 0
+              randomInt = seed.nextInt(10)
+              
+              randomInt%2 match {
+                case 0 => {
+                  senha = "123"
+                }
+                case 1 => {
+                  senha = "345"
+                }
+              }
+              
+              enviaSenha!senha
+              
+              logado = retornoLogin? ;
+              println("retornoLogin." + i + "."+logado)
+            }
+            
+            for(x <- 0 until carrinho.length){
+              println("========== CLIENTE " + i + " COMPROU " + carrinho(x)._1)
+            }
+            carrinho = List[(String,Int,Int)]()
+          }
+          case 1 => {
+            println("removerItem." + i)
+            reverteEstoqueBD!carrinho(0)._1
+            carrinho = carrinho.tail
+            if(carrinho.length == 0){
+              println("carrinhoVazio." + i)
+            }
+          }
+          case 2 => {
+            println("continuarComprando." + i)
+          }
+          
+        }
+        
+      } else {
+        //se não houver itens no carrinho, só poderá continuar navegando.
+      }
+      
       
     }
   }
-  
-  def BDLOJA(consultaProdutoBD: ?[String], respostaPrecoBD: ![Int], respostaEstoqueBD: ![Int], separaEstoqueBD: ?[String], respostaSeparaEstoqueBD: ![Boolean]) = proc {
+    
+  def BDLOJA(consultaProdutoBD: ?[String], respostaPrecoBD: ![Int], respostaEstoqueBD: ![Int], separaEstoqueBD: ?[String], 
+      respostaSeparaEstoqueBD: ![Boolean], reverteEstoqueBD: ?[String], autenticaUsuario: ?[String], enviaSenha: ?[String], retornoLogin: ![Boolean]) = proc {
     var produtoAcessado = ""
     while(true){
       alt ((true &&& consultaProdutoBD) =?=> { produto => CONSULTABD(produto, respostaPrecoBD, respostaEstoqueBD)() }
         | (true &&& separaEstoqueBD) =?=> { produto => SEPARAESTOQUEBD(produto, respostaSeparaEstoqueBD)() }
+        | (true &&& reverteEstoqueBD) =?=> { produto => REVERTEESTOQUEBD(produto)() }
+        | (true &&& autenticaUsuario) =?=> { usuario => AUTENTICAUSUARIO(usuario, enviaSenha, retornoLogin)() }
       )
     } 
   }
@@ -143,12 +216,55 @@ object lojavirtual {
              //retornar separaEstoque False!
               respostaSeparaEstoqueBD!false
             }
-          produtosBD = produtosBD.reverse
         }
   }
   
-  def ACESSOSCLIENTES(consultaProdutoBD: ![String], respostaPrecoBD: ?[Int], respostaEstoqueBD: ?[Int], separaEstoqueBD: ![String], respostaSeparaEstoqueBD: ?[Boolean]) = proc {
-    (|| (for (i <- 1 until 3) yield ACESSOCLIENTE(i,consultaProdutoBD, respostaPrecoBD, respostaEstoqueBD, separaEstoqueBD, respostaSeparaEstoqueBD)))()
+    def REVERTEESTOQUEBD(produtoConsultado: String) = proc {
+    
+      var preco = 0
+      var estoque = 0
+        //pegar a tupla do produto acessado
+        var produto = produtosBD.head
+        if(produtoConsultado == produto._1){
+          //pegar o preco do produto acessado
+          preco = produto._2
+          //pegar o estoque do produto acessado
+          estoque = produto._3
+
+            //atualizar estoque na tupla
+            var itemAtualizado = (produto._1, produto._2, produto._3 + 1)
+            //atualizar lista com tupla modificada
+            produtosBD = itemAtualizado::produtosBD.tail
+            
+        } else {
+          var produto2 = produtosBD.tail.head
+          //pegar o preco do produto acessado
+          preco = produto2._2
+          //pegar o estoque do produto acessado
+          estoque = produto2._3
+
+            //atualizar estoque na tupla
+            var itemAtualizado = (produto2._1, produto2._2, produto2._3 + 1)
+            //atualizar lista com tupla modificada
+            produtosBD = List(produto, itemAtualizado)
+        }
+  }
+  
+  def AUTENTICAUSUARIO(usuario: String, enviaSenha: ?[String], retornoLogin: ![Boolean]) = proc {
+    var login = usuario 
+    var senha = enviaSenha? ;
+    
+    //pega senha no mapa usuariosBD e compara com senha recebida
+    if(senha == usuariosBD(usuario)){
+      retornoLogin!true
+    } else {
+      retornoLogin!false
+    }
+  }  
+    
+  def ACESSOSCLIENTES(consultaProdutoBD: ![String], respostaPrecoBD: ?[Int], respostaEstoqueBD: ?[Int], separaEstoqueBD: ![String], 
+      respostaSeparaEstoqueBD: ?[Boolean], reverteEstoqueBD: ![String], autenticaUsuario: ![String], enviaSenha: ![String], retornoLogin: ?[Boolean]) = proc {
+    (|| (for (i <- 1 until 3) yield ACESSOCLIENTE(i,consultaProdutoBD, respostaPrecoBD, respostaEstoqueBD, separaEstoqueBD, respostaSeparaEstoqueBD, reverteEstoqueBD, autenticaUsuario, enviaSenha, retornoLogin)))()
   }
   
   def main(args: Array[String]) {
@@ -157,11 +273,15 @@ object lojavirtual {
     val respostaEstoqueBD = OneOne[Int]
     val separaEstoqueBD = OneOne[String]
     val respostaSeparaEstoqueBD = OneOne[Boolean]
+    val reverteEstoqueBD = OneOne[String]
+    val autenticaUsuario = OneOne[String]
+    val enviaSenha = OneOne[String]
+    val retornoLogin = OneOne[Boolean]
     
     val produtos = List("Smartphone", "TV")
     val produtosBD = List(("Smartphone", 100, 2), ("TV", 200, 1))
 
-    (ACESSOSCLIENTES(consultaProdutoBD, respostaPrecoBD, respostaEstoqueBD, separaEstoqueBD, respostaSeparaEstoqueBD) || BDLOJA(consultaProdutoBD, respostaPrecoBD, respostaEstoqueBD, separaEstoqueBD, respostaSeparaEstoqueBD))()
+    (ACESSOSCLIENTES(consultaProdutoBD, respostaPrecoBD, respostaEstoqueBD, separaEstoqueBD, respostaSeparaEstoqueBD, reverteEstoqueBD, autenticaUsuario, enviaSenha, retornoLogin) || BDLOJA(consultaProdutoBD, respostaPrecoBD, respostaEstoqueBD, separaEstoqueBD, respostaSeparaEstoqueBD, reverteEstoqueBD, autenticaUsuario, enviaSenha, retornoLogin))()
     exit
   }
   
